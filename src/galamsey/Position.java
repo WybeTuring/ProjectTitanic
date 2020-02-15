f3b1e20bb75256737d3bde875ab4b3e7b6fd55ec
Brown <persiebrown285@gmail.com>
2020-02-15 06:25:19 +0000

    Completed the Javadocs for the following classes: Galamsey.java Monitoring.java Position.java
    Please do not make any unauthorised changes to the classes.

--- src/galamsey/Position.java
+++ src/galamsey/Position.java
@@ -10,6 +10,13 @@ public class Position {
 	private double longitude;
 	
 	/**
+	 * Default Constructor
+	 */
+	public Position() {
+		
+	}
+	
+	/**
 	 * Constructs a Position object with a latitude and longitude
 	 * @param lat The latitude of the position, a double.
 	 * @param longit The longitude of the position, a double. 
@@ -52,21 +59,16 @@ public class Position {
 	}
 	
 	/**
-	 * This method compares two positions for equality by checking if their latitudes and longitudes are the same. 
-	 * @param x An object of type Position.
-	 * @return A boolean that indicates whether or not the two objects are equal. 
-	 */
-	
-	
-	
-	/**
 	 * This method returns a string that can be printed on the screen to represent a position object.
 	 * @return Returns a string that will often be printed to the console. 
 	 */
 	public String toString() {
 		return ("Position is Latitude: " + this.latitude + "  Longitude: " + this.longitude);
 	}
-
+	
+	/**
+	 * Overrides the default hashCode method
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -78,7 +80,13 @@ public class Position {
 		result = prime * result + (int) (temp ^ (temp >>> 32));
 		return result;
 	}
-
+	
+	/**
+	 * This method compares two positions for equality by checking if their latitudes and longitudes are the same. 
+	 * @param x An object of type Object.
+	 * @return A boolean that indicates whether or not the two objects are equal.
+	 * Overrides the default equals object 
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
